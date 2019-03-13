a0f7f1593d17238c6fb1ff418bd8718150eccd3b
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-13 01:05:20 -0500

    final code

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -8,13 +8,21 @@ namespace FullName
         {
             // TODO: Ask the user for their first name and then their last name.
 
-            // TODO: Call a method called FullName that returns their full name.
-
-            // TODO: Output their full name.
+            Console.Write("Please enter your first name.:");
+            string firstName = Console.ReadLine();
+            Console.Write("Please enter your last name.:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(FullName(firstName, lastName));
 
             Console.ReadLine();
         }
 
-        // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
+        public static string FullName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+            
+        }
+
+        
     }
 }
